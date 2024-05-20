78ed6a7151844707614aea6e2e8b1723df861597
table-harmony <lironkaner2007@gmail.com>
2024-05-20 11:58:07 +0300

    Init shadcn

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "kitharmony",
+  url: "https://kitharmony.vercel.app",
+  author: "tableharmony",
+  description: "starter kit",
+  links: {
+    github: "https://github.com/table-harmony/kitharmony",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
